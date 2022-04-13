e8e3c2fbacde86df8aaa347d11fc0dfed6291e11
syk1k <dambemondo@gmail.com>
2022-04-13 17:11:48 +0000

    ...

--- lib/apps/z_light/layers/widgets/layer_list_item.dart
+++ lib/apps/z_light/layers/widgets/layer_list_item.dart
@@ -9,31 +9,50 @@ class LayerListItem extends StatefulWidget {
 }
 
 class _LayerListItemState extends State<LayerListItem> {
+  bool _showActions = false;
+  _onHover(isHovering){
+    setState(() {
+      _showActions = isHovering;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(2),
-      child: Row(
-        children: [
-          Icon(Icons.remove_red_eye),
-          Expanded(
-            child: Column(
-              children: [
-                Row(
-                  children: const [
-                    Icon(Icons.image),
-                    Expanded(
-                      child: Text("Type"),
-                    ),
-                    Icon(Icons.create_outlined),
-                    Icon(Icons.delete_outlined),
-                    Icon(Icons.format_line_spacing_outlined),
-                  ],
-                ),
-              ],
+    return InkWell(
+      onHover: _onHover,
+      hoverColor: Colors.black12,
+      onTap: (){
+        //print("Tapped");
+      },
+      child: Container(
+        padding: const EdgeInsets.all(2),
+        color: Colors.black12,
+        child: Row(
+          children: [
+            const Icon(Icons.remove_red_eye),
+            Expanded(
+              child: Column(
+                children: [
+                  Row(
+                    children:[
+                      const Icon(Icons.image),
+                      const Expanded(
+                        child: Text("Type"),
+                      ),
+                      (_showActions)?
+                      Row(
+                        children: const [
+                          Icon(Icons.create_outlined),
+                          Icon(Icons.delete_outlined),
+                          Icon(Icons.format_line_spacing_outlined),
+                        ],
+                      ): Container()
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
