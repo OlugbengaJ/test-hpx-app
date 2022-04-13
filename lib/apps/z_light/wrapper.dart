064b9e839104ee50400dca5b57358e5830c6d25e
syk1k <dambemondo@gmail.com>
2022-04-13 12:00:36 +0000

    Main route

---
+++ lib/apps/z_light/wrapper.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'layers/layers.dart';
+
+
+class Wrapper extends StatefulWidget {
+  const Wrapper({ Key? key }) : super(key: key);
+
+  @override
+  State<Wrapper> createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: const [
+            Expanded(
+              flex: 1,
+              child: Layers(),
+            ),
+            Expanded(
+              flex: 8,
+              child: Layers(),
+            ),
+            Expanded(
+              flex: 1,
+              child: Layers(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
