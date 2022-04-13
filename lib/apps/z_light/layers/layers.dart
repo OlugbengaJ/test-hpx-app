3597bd031c8577d1dc0064b8f9b91942e08f7417
syk1k <dambemondo@gmail.com>
2022-04-13 16:46:53 +0000

    LayerListItems

--- lib/apps/z_light/layers/layers.dart
+++ lib/apps/z_light/layers/layers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hpx/apps/z_light/layers/widgets/layer_list_item.dart';
 
 class Layers extends StatefulWidget {
   const Layers({ Key? key }) : super(key: key);
@@ -8,10 +9,65 @@ class Layers extends StatefulWidget {
 }
 
 class _LayersState extends State<Layers> {
+  final List<Widget> _layers = [];
+  @override
+  void initState() {
+    super.initState();
+    _addLayer(); // Default layer
+  }
+  _addLayer(){
+    setState(() {
+      _layers.add(const LayerListItem());
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Layers"),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(2),
+          margin: const EdgeInsets.all(8),
+          child: const Text("Layers"),
+        ),
+        InkWell(
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            padding: const EdgeInsets.all(4),
+            margin: const EdgeInsets.all(8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text(
+                  "Create New Layer",
+                  style: TextStyle(
+                    color: Colors.black
+                  ),
+                ),
+              ],
+            ),
+          ),
+          onTap: _addLayer,
+        ),
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.all(8),
+            padding: const EdgeInsets.all(4),
+            color: Colors.black12,
+            height: MediaQuery.of(context).size.height,
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: _layers.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _layers[index];
+              }
+            )
+          ),
+        )
+      ],
     );
   }
 }
