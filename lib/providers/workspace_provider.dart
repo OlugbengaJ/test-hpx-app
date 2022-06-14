36c7f11b6dbe425bdff71b083de57999496d3edf
adelekeoadelaja <adeleke.adelaja@andela.com>
2022-06-14 16:50:28 +0300

    update the anim speed

--- lib/providers/workspace_provider.dart
+++ lib/providers/workspace_provider.dart
@@ -115,7 +115,7 @@ class WorkspaceProvider with ChangeNotifier {
   }
 
   /// [_animMillisecond] is the animation duration in milliseconds and defaults to 1s.
-  double? _animMillisecond = 1000.0;
+  double _animMillisecond = 1000.0;
 
   /// [_animationColor] is used to create a ColorTween animation.
   late Animation<Color?> _animationColor;
@@ -137,7 +137,7 @@ class WorkspaceProvider with ChangeNotifier {
   void _setAnimDuration(double? speed) {
     if (speed != null) {
       // speed exists so check if it has changed
-      final ms = speed * 50;
+      final ms = speed * (_animMillisecond / 10);
       if (ms != _animMillisecond) {
         // update the controller duration
         _animMillisecond = ms;
@@ -146,7 +146,7 @@ class WorkspaceProvider with ChangeNotifier {
     }
   }
 
-  /// [animColor] returns an animation color.
+  /// [animColor] returns a color from transitions of a start to end color.
   Color? animColor(Color beginColor, Color endColor,
       {double? speed, EnumModes? effect}) {
     if (effect == EnumModes.blinking) {
@@ -162,12 +162,12 @@ class WorkspaceProvider with ChangeNotifier {
     }
 
     _setAnimDuration(speed);
-    _checkAnimaStatus();
+    _checkAnimStatus();
 
     return _animationColor.value;
   }
 
-  /// [animColorTween] returns an animation color.
+  /// [animColorTween] returns an animation color from a tween of colors.
   Color? animColorTween(List<Color>? colors, {double? speed}) {
     if (colors == null) return null;
 
@@ -182,13 +182,13 @@ class WorkspaceProvider with ChangeNotifier {
     _animationColor = TweenSequence<Color?>(tween).animate(controller);
 
     _setAnimDuration(speed);
-    _checkAnimaStatus();
+    _checkAnimStatus();
 
     return _animationColor.value;
   }
 
-  /// [_checkAnimaStatus] this ensures the animation controller is always active.
-  void _checkAnimaStatus() {
+  /// [_checkAnimStatus] this ensures the animation controller is always active.
+  void _checkAnimStatus() {
     switch (_controller.status) {
       case AnimationStatus.completed:
         _controller.reverse();
