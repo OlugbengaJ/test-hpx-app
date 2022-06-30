fcbd4a25d75b66957088dd1d31d9006952869df3
Favour Chukwuemeka <kaosemeka@yahoo.com>
2022-06-30 19:56:06 +0100

    Tools effects (displayed shortcut keys) (#148)
    * set default colors
    
    * refactored code to using enum values
    
    * updated layer on minor error
    
    * fixed pr request error and refectored mode.dart
    
    * updated fixes fr pr
    
    * added timer to randon colors
    
    * updated code
    
    * added shortcut mode
    
    * added windows fixes
    
    * added current color for shortcut and maintined color state
    
    * fixed color picker recent colors
    
    * updated colors picker history boxes and fixed default mode for current mode
    
    * reset preset value for mood
    
    * set effect name for default
    
    * updated picker dropdown to choose picker
    
    * added layers update on change in tools and effects
    
    * added layer notification
    
    * added icon fix on for modeprovider and improved functionality for effects
    
    * merge remove merge extra function delcalration
    
    * fixed slider error
    
    * fixed the image mode back
    
    * fixed color cycle errpr
    
    * fixed build error
    
    * added notification on leaving interactove
    
    * removed the hello world popup
    
    * extracted colors from images as a list
    
    * extract colors and set to current mode and effect to image
    
    * updated image effect
    
    * generate ewxtracted matrix
    
    * added extracted color matrix to model and set as current effect
    
    * added submode choice to mode provider
    
    * fixed wave knob
    
    * fixed mode switch
    
    * in all effects modefixed notift errors
    
    * fixed image state
    
    * fixed images error
    
    * set current effect inmode provider on image change
    
    * optimized image mode imaplementation
    
    * adding fixes
    
    * fixed merge conflict
    
    * fixed breathing error
    
    * fixed extract color current color interfcae
    
    * fixed bugs and errors in color picker, image, degrees etc
    
    * removed flat buttons
    
    * updated image mode
    
    * updated image effect
    
    * added image fixes
    
    * added shortcut mode notifier
    
    * refactored mode code
    
    * added shortcut fix generate to mode and fixed mode code
    
    * added layer notify
    
    * updated color border highlight
    
    * reset mode to current mode on change layers
    
    * fixed image state error
    
    * contact support widget added
    
    * added contact support dialog
    
    * fixed random color error
    
    * updated mode data
    
    * fixed contact support logic
    
    * added shortcut fixes
    
    Co-authored-by: Favour Chukwuemeka <chikeluba.chukwuemeka@andela.com>
    Co-authored-by: Philemon Gumah <48827922+phil196000@users.noreply.github.com>

--- lib/providers/tools_effect_provider/widget/shortcut_widget_provider.dart
+++ lib/providers/tools_effect_provider/widget/shortcut_widget_provider.dart
@@ -15,61 +15,53 @@ class ShortcutWidgetProvider extends ChangeNotifier {
 
   /// function to generate the shortcut widget with the text controller and input field
   /// also sets the default input field if passed as an argument in the function or defaults to 'esc'
-  addNewCommand(String labelInput, String textinput) {
+  addNewCommand(String textinput) {
     TextEditingController inputController = TextEditingController();
     inputController.text = (textinput != '') ? textinput : " ";
     keyController.add(inputController);
 
-    TextEditingController labelControl = TextEditingController();
-    labelControl.text = (labelInput != '') ? labelInput : "Enter label ... ";
-    labelController.add(labelControl);
-
-    commandsList.add(Container(
-        decoration: BoxDecoration(color: Colors.grey.shade800),
-        padding: const EdgeInsets.only(top: 5, bottom: 5, left: 10),
-        margin: const EdgeInsets.only(top: 5, left: 0, right: 10),
-        child: Row(children: [
-          Expanded(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Enter Text', textAlign: TextAlign.left, style: labelStyle),
-              // SizedBox(
-              //     width: 100,
-              //     height: 25,
-              //     child: TextField(
-              //         controller: labelController.last,
-              //         obscureText: false,
-              //         decoration: const InputDecoration(
-              //           border: OutlineInputBorder(
-              //               borderRadius: BorderRadius.all(Radius.zero)),
-              //           contentPadding:
-              //               EdgeInsets.symmetric(vertical: 0, horizontal: 5),
-              //           hintStyle: TextStyle(color: Colors.white),
-              //         ),
-              //         style: const TextStyle(fontSize: 14)))
-            ],
-          )),
-          Expanded(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                  width: 100,
-                  height: 25,
-                  child: TextField(
-                      controller: keyController.last,
-                      obscureText: false,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.zero)),
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 0, horizontal: 5),
-                        hintStyle: TextStyle(color: Colors.white),
-                      ),
-                      style: const TextStyle(fontSize: 14)))
-            ],
-          ))
-        ])));
+    return Row(children: [
+      Expanded(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Enter Text', textAlign: TextAlign.left, style: labelStyle),
+          // SizedBox(
+          //     width: 100,
+          //     height: 25,
+          //     child: TextField(
+          //         controller: labelController.last,
+          //         obscureText: false,
+          //         decoration: const InputDecoration(
+          //           border: OutlineInputBorder(
+          //               borderRadius: BorderRadius.all(Radius.zero)),
+          //           contentPadding:
+          //               EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+          //           hintStyle: TextStyle(color: Colors.white),
+          //         ),
+          //         style: const TextStyle(fontSize: 14)))
+        ],
+      )),
+      Expanded(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+              width: 150,
+              height: 25,
+              child: TextField(
+                  controller: keyController.last,
+                  obscureText: false,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.zero)),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                  style: const TextStyle(fontSize: 14)))
+        ],
+      ))
+    ]);
   }
 }
