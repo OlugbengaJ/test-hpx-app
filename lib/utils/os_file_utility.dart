3f40ccf229fc84a89896847013c213f36b39f80a
adelekeoadelaja <adeleke.adelaja@andela.com>
2022-07-13 00:51:42 +0300

    display file image

--- lib/utils/os_file_utility.dart
+++ lib/utils/os_file_utility.dart
@@ -153,14 +153,23 @@ class OSFileUtility {
 
             widgets.addAll(appInfo.entries.map((e) {
               if (e.key.toLowerCase() == 'icon') {
-                return Container(
-                  margin: EdgeInsets.zero,
-                  child: Image.memory(
-                    File(e.value).readAsBytesSync(),
-                    width: 100.0,
-                  ),
+                // get icon file
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.zero,
+                      child: Image.memory(
+                        File(e.value).readAsBytesSync(),
+                        width: 50.0,
+                        height: 50.0,
+                      ),
+                    ),
+                    Text('${e.key}: ${e.value}'),
+                  ],
                 );
               }
+
               return Text('${e.key}: ${e.value}');
             }));
             workspaceProvider.toggleModal(widgets);
