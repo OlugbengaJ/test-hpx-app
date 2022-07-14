016ed0add0aeeb1cacfbad80fdc87946a4f5aabe
Favour Chukwuemeka <chikeluba.chukwuemeka@andela.com>
2022-07-14 05:11:55 -0700

    added walkthrough modal ui

--- lib/providers/tutorial_provider/tutorial_provider.dart
+++ lib/providers/tutorial_provider/tutorial_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:hpx/utils/constants.dart';
 import 'package:hpx/widgets/theme.dart';
 import 'package:just_the_tooltip/just_the_tooltip.dart';
@@ -18,6 +17,7 @@ class TooltipTutorialProvider extends ChangeNotifier {
   final tooltipController = JustTheController();
 
   bool showTutorial = false;
+  Widget tutorial = Container();
   var direction = AxisDirection.down;
   double width = 300;
   String? toolTipToShow;
@@ -89,102 +89,6 @@ class TooltipTutorialProvider extends ChangeNotifier {
     }
     toolTipToShow = null;
     // notifyListeners()
-  }
-
-  generateDialogTutorial(BuildContext context) {
-    showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (context) {
-          return SimpleDialog(backgroundColor: const Color(0xff121212),
-              // contentPadding: const EdgeInsets.only(
-              //     top: 40, left: 20, right: 20, bottom: 30),
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(top: 40, bottom: 10),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 1)),
-                  width: 900,
-                  height: 500,
-                  child: Column(children: [
-                    Text(
-                      'Welcome to Z Light Space',
-                      style: h1Style,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(top: 20),
-                    ),
-                    Text(
-                      'Personalize your workspace lightning to enhance your creative workflow',
-                      style: pStyle,
-                    ),
-                    Container(
-                      width: 200,
-                      height: 200,
-                      margin: const EdgeInsets.only(top: 70, bottom: 10),
-                      child: Image.asset(
-                        Constants.zLightIconPng,
-                        height: 200,
-                        width: 200,
-                      ),
-                    ),
-                    Container(
-                      width: 250,
-                      margin: const EdgeInsets.only(top: 50),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: [
-                                TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    style: textBtnStyleWhite,
-                                    child: SizedBox(
-                                        height: 40,
-                                        child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            children: const [
-                                              Padding(
-                                                  padding: EdgeInsets.all(8.0),
-                                                  child: Text('Lets Go'))
-                                            ]))),
-                              ],
-                            ),
-                          ),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: [
-                                TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    style: textBtnStyleBlack,
-                                    child: SizedBox(
-                                        height: 40,
-                                        child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            children: const [
-                                              Padding(
-                                                  padding: EdgeInsets.all(8.0),
-                                                  child: Text('Skip Tutorial'))
-                                            ]))),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ]),
-                ),
-              ]);
-        });
   }
 }
 
